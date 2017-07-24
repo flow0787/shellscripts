69a96834a0f39d37e588487c39c9daa09cd69c9c
florin <badeaflorien@gmail.com>
2017-07-24 11:50:45 +0300

    added colors to changerootpass.sh

--- changerootpass.sh
+++ changerootpass.sh
@@ -2,9 +2,11 @@
 #Script to change root password with a backup of shadow file#
 
 clear;
+RED='\033[0;31m'
+NC='\033[0m' 
 new_pass=$(date | md5sum | awk {'print $1'})
 echo -e "=======================================================\n"
-echo -e "     NEW PASSWORD: "$new_pass "\n"
+echo -e "     NEW PASSWORD: "${RED}$new_pass${NC} "\n"
 backup_shadow=/etc/shadow.`date +"%b-%d-%Y.Time_%I.%M.%S_%p"`;
 
 cp /etc/shadow $backup_shadow;
