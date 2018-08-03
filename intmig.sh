68274286cd6d690a5aa88959e38fe2edf2cae824
Flo <Flo@Roxs-MacBook-Pro.local>
2018-08-03 16:36:09 +0300

    converted disk_usage to int, fixed chmod and a few echo's

--- intmig.sh
+++ intmig.sh
@@ -16,6 +16,8 @@ NC='\033[0m'
 user=$1
 destination=$2
 disk_usage=$(du -sh /home/$user | awk {'print $1'} | sed 's/G//g'| sed 's/M//g')
+#APROXIMATE TO INTEGER
+disk_usage=$(printf "%.0f" $disk_usage)
 primary_domain=$(grep $user /etc/trueuserdomains | cut -d : -f 1)
 
 #IF USER EXISTS ON THE SERVER 
@@ -24,11 +26,11 @@ if grep -q $user /etc/trueuserowners; then
 	#IF WE HAVE USER AND SERVER AS ARGUMENTS
 	if [[ $# -eq 2 ]]; then
 	#If account's disk usage below 25 GB
-		if [[ $disk_usage -le 25 ]]; then
+		if [[ "$disk_usage" -le 25 ]]; then
 			echo -e "${GREEN}Disk usage under 25GB, starting pkgacct:${NC}"
 			/scripts/pkgacct $user;
 			chown $user:$user /home/cpmove-$user.tar.gz;
-			chmod 644 $user:$user /home/cpmove-$user.tar.gz;
+			chmod 644 /home/cpmove-$user.tar.gz;
 			mv /home/cpmove-$user.tar.gz /home/$user/public_html;
 			echo -en "${DARK_BLUE}Your download URL:${NC} ";
 			echo "http://$primary_domain/cpmove-tar.gz"
@@ -38,20 +40,16 @@ if grep -q $user /etc/trueuserowners; then
 			echo -e "${RED}Disk usage over 25 GB, packaging account without home:${NC}"
 			/scripts/pkgacct --skiphomedir $user;
 			chown $user:$user /home/cpmove-$user.tar.gz;
-			chmod 644 $user:$user /home/cpmove-$user.tar.gz;
+			chmod 644 /home/cpmove-$user.tar.gz;
 			mv /home/cpmove-$user.tar.gz /home/$user/public_html;
 			echo -en "${DARK_BLUE}Your download URL:${NC} ";
 			echo "http://$primary_domain/cpmove-tar.gz"
-			echo -e "${GREEN}SHELL ENABLED! Disable once rsync is done!${NC}";
+			echo -e "${GREEN}SHELL ENABLED! Disable SHELL once rsync is done!${NC}";
 			chsh -s /bin/bash $user;
-			echo -en "{$DARK_BLUE}Your rsync command:${NC} ";
-			echo "rsync -auHP /home/$user $user@$destination.hostpapa.com:/home/$user"
+			echo -en "${DARK_BLUE}Your rsync command:${NC} ";
+			echo "rsync -auHP /home/$user/ $user@$destination.hostpapa.com:/home/$user/"
 		fi
-		read -n 1 -s -r -p "Press any key to remove the cPanel backup..."
-		while true
-		do
-			rm -vf /home/$user/public_html/cpmove-$user.tar.gz
-		done
+		echo -en "${RED}Remember to remove:${NC} /home/$user/public_html/cpmove-tar.gz"
 	else
 		echo -e "${DARK_BLUE}This script requires 2 arguments (user and server shortname)!${NC}"
 	fi
