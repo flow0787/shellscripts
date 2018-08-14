577e4a69692524f763ee0af2c380dbf58c11133f
FlorinB <badeaflorien@gmail.com>
2018-08-14 16:44:13 +0300

    fixed 1 2 3 4

--- intmig.sh
+++ intmig.sh
@@ -26,12 +26,13 @@ disk_usage=$(du -sh /home/$user | awk {'print $1'} | sed 's/G//g'| sed 's/M//g')
 #APROXIMATE TO INTEGER
 disk_usage=$(printf "%.0f" $disk_usage)
 primary_domain=$(grep $user /etc/trueuserdomains | cut -d : -f 1)
+current_server=$(hostname)
 
-#IF USER EXISTS ON THE SERVER 
-if grep -q $user /etc/trueuserowners; then
 
-	#IF WE HAVE USER AND SERVER AS ARGUMENTS
-	if [[ $# -eq 2 ]]; then
+#IF WE HAVE USER AND SERVER AS ARGUMENTS
+if [[ $# -eq 2 ]]; then
+	#IF USER EXISTS ON THE SERVER 
+	if grep -q $user /etc/trueuserowners; then
 	#If account's disk usage below 25 GB
 		if [[ "$disk_usage" -le 25 ]]; then
 			echo -e "${GREEN}Disk usage under 25GB, starting pkgacct:${NC}"
@@ -50,17 +51,18 @@ if grep -q $user /etc/trueuserowners; then
 			chmod 644 /home/cpmove-$user.tar.gz;
 			mv /home/cpmove-$user.tar.gz /home/$user/public_html;
 			echo -en "${DARK_BLUE}Your download URL:${NC} ";
-			echo "http://$primary_domain/cpmove-tar.gz"
+			echo "http://$primary_domain/cpmove-$user.tar.gz"
+			echo -en "${DARK_BLUE}Hostname-based download URL:${NC} "
+			echo "$http://$current_server/~$user/cpmove-$user.tar.gz"
 			echo -e "${GREEN}SHELL ENABLED! Disable SHELL once rsync is done!${NC}";
 			chsh -s /bin/bash $user;
 			echo -en "${DARK_BLUE}Your rsync command:${NC} ";
-			echo "rsync -auHP /home/$user/ $user@$destination.hostpapa.com:/home/$user/"
+			echo "rsync -auHP $user@$destination.hostpapa.com:/home/$user/ /home/$user/"
 		fi
-		echo -en "${RED}Remember to remove:${NC} /home/$user/public_html/cpmove-$user.tar.gz"
-	else
-		echo -e "${DARK_BLUE}This script requires 2 arguments (user and server shortname)!${NC}"
-	fi
-
-else
+		echo -e "${RED}Remember to remove:${NC} /home/$user/public_html/cpmove-$user.tar.gz"
+		else
 	echo -e "${RED}$user DOES NOT EXIST ON THIS SERVER! Aborting...${NC}";
+	fi
+	else
+	echo -e "${DARK_BLUE}This script requires 2 arguments (user and server shortname)!${NC}"
 fi
