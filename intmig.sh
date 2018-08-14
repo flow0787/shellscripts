3d9f09d6da77a41ef1de824e9c6be2c0e87c73ef
FlorinB <badeaflorien@gmail.com>
2018-08-14 16:47:15 +0300

    fixed 1 2 3 4

--- intmig.sh
+++ intmig.sh
@@ -30,39 +30,42 @@ current_server=$(hostname)
 
 
 #IF WE HAVE USER AND SERVER AS ARGUMENTS
-if [[ $# -eq 2 ]]; then
-	#IF USER EXISTS ON THE SERVER 
-	if grep -q $user /etc/trueuserowners; then
-	#If account's disk usage below 25 GB
-		if [[ "$disk_usage" -le 25 ]]; then
-			echo -e "${GREEN}Disk usage under 25GB, starting pkgacct:${NC}"
-			/usr/local/cpanel/bin/cpuwatch 8 /scripts/pkgacct $user;
-			chown $user:$user /home/cpmove-$user.tar.gz;
-			chmod 644 /home/cpmove-$user.tar.gz;
-			mv /home/cpmove-$user.tar.gz /home/$user/public_html;
-			echo -en "${DARK_BLUE}Your download URL:${NC} ";
-			echo "http://$primary_domain/cpmove-tar.gz"
+if [[ $# -lt 2 ]]; then
 
-		#If usage above 25 GB skip the home folder
-		else
-			echo -e "${RED}Disk usage over 25 GB, packaging account without home:${NC}"
-			/usr/local/cpanel/bin/cpuwatch 8 /scripts/pkgacct --skiphomedir $user;
-			chown $user:$user /home/cpmove-$user.tar.gz;
-			chmod 644 /home/cpmove-$user.tar.gz;
-			mv /home/cpmove-$user.tar.gz /home/$user/public_html;
-			echo -en "${DARK_BLUE}Your download URL:${NC} ";
-			echo "http://$primary_domain/cpmove-$user.tar.gz"
-			echo -en "${DARK_BLUE}Hostname-based download URL:${NC} "
-			echo "$http://$current_server/~$user/cpmove-$user.tar.gz"
-			echo -e "${GREEN}SHELL ENABLED! Disable SHELL once rsync is done!${NC}";
-			chsh -s /bin/bash $user;
-			echo -en "${DARK_BLUE}Your rsync command:${NC} ";
-			echo "rsync -auHP $user@$destination.hostpapa.com:/home/$user/ /home/$user/"
-		fi
-		echo -e "${RED}Remember to remove:${NC} /home/$user/public_html/cpmove-$user.tar.gz"
-		else
-	echo -e "${RED}$user DOES NOT EXIST ON THIS SERVER! Aborting...${NC}";
-	fi
+echo -e "${DARK_BLUE}This script requires 2 arguments (user and server shortname)!${NC}"
+exit 1
+
+fi
+
+#IF USER EXISTS ON THE SERVER 
+if grep -q $user /etc/trueuserowners; then
+#If account's disk usage below 25 GB
+	if [[ "$disk_usage" -le 25 ]]; then
+		echo -e "${GREEN}Disk usage under 25GB, starting pkgacct:${NC}"
+		/usr/local/cpanel/bin/cpuwatch 8 /scripts/pkgacct $user;
+		chown $user:$user /home/cpmove-$user.tar.gz;
+		chmod 644 /home/cpmove-$user.tar.gz;
+		mv /home/cpmove-$user.tar.gz /home/$user/public_html;
+		echo -en "${DARK_BLUE}Your download URL:${NC} ";
+		echo "http://$primary_domain/cpmove-tar.gz"
+
+	#If usage above 25 GB skip the home folder
 	else
-	echo -e "${DARK_BLUE}This script requires 2 arguments (user and server shortname)!${NC}"
+		echo -e "${RED}Disk usage over 25 GB, packaging account without home:${NC}"
+		/usr/local/cpanel/bin/cpuwatch 8 /scripts/pkgacct --skiphomedir $user;
+		chown $user:$user /home/cpmove-$user.tar.gz;
+		chmod 644 /home/cpmove-$user.tar.gz;
+		mv /home/cpmove-$user.tar.gz /home/$user/public_html;
+		echo -en "${DARK_BLUE}Your download URL:${NC} ";
+		echo "http://$primary_domain/cpmove-$user.tar.gz"
+		echo -en "${DARK_BLUE}Hostname-based download URL:${NC} "
+		echo "$http://$current_server/~$user/cpmove-$user.tar.gz"
+		echo -e "${GREEN}SHELL ENABLED! Disable SHELL once rsync is done!${NC}";
+		chsh -s /bin/bash $user;
+		echo -en "${DARK_BLUE}Your rsync command:${NC} ";
+		echo "rsync -auHP $user@$destination.hostpapa.com:/home/$user/ /home/$user/"
+	fi
+	echo -e "${RED}Remember to remove:${NC} /home/$user/public_html/cpmove-$user.tar.gz"
+	else
+echo -e "${RED}$user DOES NOT EXIST ON THIS SERVER! Aborting...${NC}";
 fi
