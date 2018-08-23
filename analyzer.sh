de85bb38efbbfdcd9a15f7482c7e8e59f99221a9
Flo <Flo@Roxs-MacBook-Pro.local>
2018-08-23 14:15:53 +0300

    analyzer 2nd commit

--- analyzer.sh
+++ analyzer.sh
@@ -16,7 +16,7 @@ month=$2
 echo " === General Info =============";
 echo -en "username\t: $user \n";
 echo -en "hosting plan\t: " ; grep PLAN /var/cpanel/users/$user | cut -d = -f2
-
+echo ;
 
 #Getting domain information
 echo " === Domains Information ======";
@@ -24,12 +24,12 @@ echo -en "primary\t: "; grep $user /etc/trueuserdomains | cut -d : -f1;
 echo -en "addons\t: "; sed -n '/addon_domains:/,/main_domain:/p' /var/cpanel/userdata/$user/main | grep -v addon_domains | grep -v main_domain |wc -l;
 echo -en "subs\t: "; sed -n '/sub_domains:/,//p' /var/cpanel/userdata/$user/main | grep -v sub_domains:| wc -l;
 echo -en "parked\t: "; sed -n '/parked_domains:/,/sub_domains:/p' /var/cpanel/userdata/$user/main | grep -v parked_domains | grep -v sub_domains |wc -l
-
+echo ;
 
 #Getting CloudLinux faults/info for the past 10 days for the account
 echo " === CL faults for past 10 days ===========================";
 lveinfo --user $user --period 10d --time-unit 1d --show-columns From To NprocF EPf VMemF PMemF CPUf IOf IOPSf
-
+echo;
 
 #Getting top 3 most intensive domains for the entire month
 echo " === Top 3 Active Domains ===============================";
@@ -37,12 +37,12 @@ for i in $(ls -lahS logs/ | grep $month | head -3 | awk {'print $9'})
 do 
 		echo -en $i|cut -d - -f1; zcat logs/$i|wc -l
 done
-
+echo ;
 
 #Getting top 10 IPs and user agent for all domains for the last 5 days
 echo " === Top 10 IP Addresses ================================";
 zcat logs/*-$month-* |awk -vDate=`date -d'now-5 days' +[%d/%b/%Y:%H:%M:%S` ' { if ($4 > Date) print $1, $12, $18}' | sort | uniq -c | sort -fr | head
-
+echo ;
 
 #Getting top 10 IPs and user agent for top 3 most intensive domains for past 5 days
 echo " === Top 10 IPs For Top 5 Sites For Past 5 Days =========";
@@ -50,10 +50,8 @@ for i in $(ls -lahS logs/ | grep $month | head -3 | awk {'print $9'})
 do 
 		echo -en "=== " $i|cut -d - -f1; zcat logs/$i |awk -vDate=`date -d'now-5 days' +[%d/%b/%Y:%H:%M:%S` ' { if ($4 > Date) print $1, $12, $18}' | sort | uniq -c | sort -fr | head; echo -en "\n";
 done
-
+echo ;
 
 #Getting top 10 most accessed content for all sites for past 5 days
 echo " === Top 10 Most Accessed Content/Files =================="; 
 zcat logs/*-$month-* |awk -vDate=`date -d'now-5 days' +[%d/%b/%Y:%H:%M:%S` ' { if ($4 > Date) print $7}' | sort | uniq -c | sort -fr | head
-
-zcat logs/*-Aug*|egrep "20/Aug|21/Aug|22/Aug|23/Aug|24/Aug|25/Aug"|awk '{print $7}' | cut -d? -f1|sort|uniq -c|sort -nk1|tail -n10 | sort -rnk1
