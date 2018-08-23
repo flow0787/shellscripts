f3ab91199ad376e9ddfd9dff2a0d57d4934cff8d
Flo <Flo@Roxs-MacBook-Pro.local>
2018-08-23 17:54:12 +0300

    pretty prints and fix for top content

--- analyzer.sh
+++ analyzer.sh
@@ -52,7 +52,7 @@ function top_three(){
 	echo " === Top 3 Active Domains ===============================";
 	for i in $(ls -hS $path | grep gz| head -3)
 	do 
-			echo -en $i|cut -d - -f1; zcat $path/$i|wc -l
+			echo -e $(echo -en $i|cut -d - -f1) " \t : \t"$(zcat logs/$i|wc -l)
 	done
 	echo ;
 }
@@ -85,7 +85,7 @@ function top_ten_content(){
 	if [[ $(zcat $path/* |awk -vDate=`date -d'now-5 days' +[%d/%b/%Y:%H:%M:%S` ' { if ($4 > Date) print $7}' | sort | uniq -c | sort -fr | head | wc -l) -eq "0" ]]; then
 		echo "There is no traffic to show for the past 5 days ... ";
 	else
-		zcat $path/* |awk -vDate=`date -d'now-5 days' +[%d/%b/%Y:%H:%M:%S` ' { if ($4 > Date) print $7}' | sort | uniq -c | sort -fr | head
+		zcat $path/* |awk -vDate=`date -d'now-5 days' +[%d/%b/%Y:%H:%M:%S` ' { if ($4 > Date) print $7}'|sed 's/?.*//' | sort | uniq -c | sort -fr | head
 	fi
 }
 
