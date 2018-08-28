53556c0b6c662836aa7d9872fbaccbde4d94e43e
Flo <Flo@Roxs-MacBook-Pro.local>
2018-08-28 14:15:50 +0300

    added in progress check howto and next steps

--- ftp.sh
+++ ftp.sh
@@ -11,6 +11,7 @@
 # References      : N/A
 #=================================================================================#
 
+#Clear the screen and define colors
 clear;
 BLUE='\033[0;34m'
 DARK_RED='\033[1;31m'
@@ -21,6 +22,7 @@ GREEN='\033[0;32m'
 echo -e "${BLUE}-=====BEGIN FTP SITE TRANSFER=====-${NC}"
 
 
+#Test if transfer folder exists and start downloading
 if [ ! -d transfer ]; then
 	mkdir transfer; cd transfer;
 	read -p "FTP user: " ftp_user;
@@ -33,7 +35,11 @@ else
 	echo -e "${DARK_RED}transfer directory already exists:${NC}"
 	ls -lah transfer
 fi
+
+#Wait 3 seconds
 sleep 3;
+
+#Check if download went well
 if grep -q saved wget-log ; then
 	echo
 	echo -e "${GREEN}Transfer is now in progress:${NC}"
@@ -46,3 +52,10 @@ else
 	tail -50 wget-log
 	echo "-=====END FTP SITE TRANSFER=====-"
 fi
+
+echo -e "${BLUE}Check progress by running:${NC}"
+echo "tail ~/transfer/wget-log"
+echo -e "${GREEN}\t\t-=====Next Steps=====- ${NC}
+1. Transfer Database (can use https://www.adminer.org/ if no control panel access)
+2. Move site files & folders / Add domain on the account (if needed)
+3. Import Database & adjust database connection files"
